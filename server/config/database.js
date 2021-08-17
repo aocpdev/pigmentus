c841f166327aba49939d620d950e504697c4f659
Axel Castro <axel@prsoft.com>
2021-08-17 11:02:02 -0400

    Added some changes

--- server/config/database.js
+++ server/config/database.js
@@ -4,9 +4,9 @@ const { Pool } = require('pg');
 
 
 
-const { DATABASE_URL, DB_USER,DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE, DB_USER } = require('./keys')
+const { DATABASE_URL, DB_USER,DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = require('./keys')
 
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 
