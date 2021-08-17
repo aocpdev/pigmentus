f31f32311b73bf5fe39e40ab7eec9b4496c6eb58
Axel Castro <axel@prsoft.com>
2021-08-17 13:26:22 -0400

    Added some changes

--- server/app.js
+++ server/app.js
@@ -24,11 +24,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Express Session
+// app.use(session({
+//     secret: JWT_KEY,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+
 app.use(session({
-    secret: JWT_KEY,
-    resave: false,
-    saveUninitialized: false
-}))
+  store: new (require('connect-pg-simple')(session))(),
+  secret: process.env.FOO_COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+}));
 
 
 // Routes
