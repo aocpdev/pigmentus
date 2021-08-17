5c24d73f03281c5d16285e33646a85a79b12ac14
Axel Castro <axel@prsoft.com>
2021-08-17 14:31:43 -0400

    Added some changes

--- server/app.js
+++ server/app.js
@@ -24,19 +24,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Express Session
+// app.use(session({
+//     secret: JWT_KEY,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+
 app.use(session({
+    store: new (require('connect-pg-simple')(session))(),
     secret: JWT_KEY,
     resave: false,
-    saveUninitialized: false
-}))
-
-// app.use(session({
-//     store: new (require('connect-pg-simple')(session))(),
-//     secret: 'secret',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
-// }));
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+}));
 
 
 // Routes
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.HEROKU_POSTGRESQL_CHARCOAL_URL,
     DB_PORT: process.env.DB_PORT,
     JWT_KEY: process.env.JWT_KEY,
     NODE_ENV: process.env.NODE_ENV
