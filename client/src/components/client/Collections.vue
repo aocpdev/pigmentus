880c1f250b637f8c8c9a17f9a85c101c95e9f4d7
Axel Castro <axel@prsoft.com>
2021-08-17 18:02:43 -0400

    Added some Changes in UI

--- client/src/components/client/Collections.vue
+++ client/src/components/client/Collections.vue
@@ -40,7 +40,7 @@
                                     <v-col  class="py-0 my-0" align="start" style="text-align: start" v-if="item.isDealDay">
                                         <svg align="start" style="width:40px;height:40px; color: red" viewBox="0 0 24 24">
                                             <path fill="currentColor" d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
-                                            <text x="3" y="14" class="percentage">-{{item.specialOfferPercentage}}%</text>
+                                            <text x="3" y="14" class="percentage" transform="rotate(46, 14, 13) translate(2,2)">-{{item.specialOfferPercentage}}%</text>
                                         </svg>
                                     </v-col>
                                     <!-- <v-col cols="12" v-if="false">
@@ -166,7 +166,7 @@ export default {
 
     .percentage {
         fill: white;
-        font-size: 8px;
+        font-size: 6.5px;
         font-weight: bold
     }
 
