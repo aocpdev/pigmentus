8fceb55b9a66419d3517dc642e7240c9934cf787
Axel Castro <axel@prsoft.com>
2021-08-17 13:40:20 -0400

    Added some changes

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   store: new (require('connect-pg-simple')(session))(),
-  secret: process.env.FOO_COOKIE_SECRET,
+  secret: 'secret',
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
--- server/config/database.js
+++ server/config/database.js
@@ -11,7 +11,8 @@ const isProduction = NODE_ENV === 'production';
 const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 
 const pool = new Pool ({
-    connectionString: isProduction ? DATABASE_URL : connectionString
+    connectionString: isProduction ? DATABASE_URL : connectionString,
+    ssl: true
 })
 
 module.exports = {
