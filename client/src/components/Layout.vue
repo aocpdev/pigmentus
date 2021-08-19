46303c9191c4cf55e5a1583bc21bfc9c93372838
Axel Castro <axel@prsoft.com>
2021-08-19 03:38:42 -0400

    Added some Changes in UI

--- client/src/components/Layout.vue
+++ client/src/components/Layout.vue
@@ -269,19 +269,23 @@
 
           <ul>
             <li>
-              <a href="https://www.facebook.com/Pigmentus"
-                ><i class="fab fa-facebook" aria-hidden="true"></i
-              ></a>
+              <a href="https://www.facebook.com/Pigmentus">
+                <svg style="width:26px;height:26px" viewBox="0 0 24 24">
+                    <path id="facebook" fill="#000" d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z" />
+                </svg>
+              </a>
             </li>
-            <li>
+            <!-- <li>
               <a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
-            </li>
+            </li> -->
             <!-- <li><a href="#"><i class="fab fa-google-plus-g" aria-hidden="true"></i></a></li> -->
             <!-- <li><a href="#"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li> -->
             <li>
-              <a href="https://www.instagram.com/pigmentus_pr/"
-                ><i class="fab fa-instagram" aria-hidden="true"></i
-              ></a>
+              <a href="https://www.instagram.com/pigmentus_pr/">
+                <svg style="width:26px;height:26px" viewBox="0 0 24 24">
+                    <path id="instagram" fill="#000" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
+                </svg>
+              </a>
             </li>
           </ul>
         </v-card-title>
@@ -448,23 +452,17 @@ ul li a .fab {
   transition: 0.5s;
 }
 
-ul li:nth-child(1) a:hover .fab {
-  color: #3b5998;
+ul li:nth-child(1) a:hover #facebook {
+  fill: #3b5998;
 }
 
-ul li:nth-child(2) a:hover .fab {
-  color: #00aced;
+ul li:nth-child(2) a:hover #instagram {
+  fill: #e4405f;
 }
 
-ul li:nth-child(3) a:hover .fab {
-  color: #dd4b39;
-}
 
-ul li:nth-child(4) a:hover .fab {
-  color: #007bb6;
-}
 
-ul li:nth-child(5) a:hover .fab {
-  color: #e4405f;
-}
+/* #facebook:hover {
+  fill: #3b5998;
+} */
 </style>
