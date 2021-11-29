1c05656177eec75b172c71814f91dfa921f95213
Axel Castro <axel@prsoft.com>
2021-11-28 19:39:11 -0500

    Added google analytics

--- client/src/views/Signin.vue
+++ client/src/views/Signin.vue
@@ -106,7 +106,7 @@ export default {
             preferences: res.data.preferences,
             roleId: res.data.roleId
           }
-          Vue.$gtag.set('user_id', res.data.id);
+          // Vue.$gtag.set('user_id', res.data.id);
 
           // this.$gtag.set( {'user_id': res.data.id}); // Set the user ID using signed-in user_id.
 
