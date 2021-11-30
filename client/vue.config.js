d7907c939b6b8f2b1f7e9af5300be1aff9581a28
Axel Castro <axel@prsoft.com>
2021-11-29 22:03:02 -0500

    Added google analytics

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,14 @@
+// require = require('esm')(module);
+// const { routes } = require('./src/router/index');
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  // pluginOptions: {
+  //   sitemap: {
+  //       baseURL: 'https://pigmentus.herokuapp.com/home',
+  //       routes,
+  //   }
+  // }
 }
