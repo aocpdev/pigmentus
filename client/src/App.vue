4ac2f8fddf01ae43ea95f723ba3ad3ed8b0db316
Axel Castro <axel@prsoft.com>
2021-11-29 20:30:29 -0500

    Added google analytics

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,7 +18,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'description', content: 'Microblading by Pigmentus is a beauty academy and store with Headquarter in Puerto Rico. Best Microblading in Puerto Rico.' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { property: 'og:title', content: 'Microblading by Pigmentus Academy ← Pigmentus Puerto Rico'},
+      { property: 'og:site_name', content: 'Pigmentus Puerto Rico'},
+      { property: 'og:type', content: 'website'},
+
+      // Google / Schema.org markup:
+      { itemprop: 'name', content: 'Microblading by Pigmentus Academy ← Pigmentus Puerto Rico'},
+      { itemprop: 'description', content: 'Microblading by Pigmentus is a beauty academy and store with Headquarter in Puerto Rico. Best Microblading in Puerto Rico.'},
     ]
 
   },
